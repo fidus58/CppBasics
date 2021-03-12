7834d1ac2951b8a5c9bb2fe2cd4ac120038ec172
Klaus Ahrens <ahrens@informatik.hu-berlin.de>
2021-03-12 14:28:51 +0100

    some edits

--- basics/A.hpp
+++ basics/A.hpp
@@ -5,17 +5,6 @@
 //  Created by Klaus Ahrens on 12.03.21.
 //
 
-/*
-  Demo of:
-    - separate compilation
-    - header guards
-    - include ALL needed headers
-    - define all member data
-    - declare at least the member functions
-    - define short (inline) member functions
-    - header and impl files
-*/
-
 #ifndef A_hpp
 #define A_hpp
 
