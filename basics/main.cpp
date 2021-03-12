f40a22e0e2c80a89373951e2302a9d2f8628c542
Klaus Ahrens <ahrens@informatik.hu-berlin.de>
2021-03-12 10:33:20 +0100

    Initial Commit

---
+++ basics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  basics
+//
+//  Created by Klaus Ahrens on 12.03.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
